116062f83c7f3cc7fa1cd593a9204c607de19ed5
Jan Cyril Segubience <jancyrilsegubience@gmail.com>
2016-11-20 10:34:48 +0000

    Admin login assets

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,39 @@ require('laravel-elixir-vue-2');
  */
 
 elixir(mix => {
-    mix.sass('app.scss')
-       .webpack('app.js');
+    // Admin Login Assets
+    mix.styles([
+        'bootstrap/dist/css/bootstrap.css',
+        'fontawesome/css/font-awesome.css',
+        'AdminLTE/dist/css/AdminLTE.css',
+        'AdminLTE/plugins/iCheck/square/blue.css',
+        'pnotify/dist/pnotify.css'
+    ], 'public/css/login.css', 'bower_components/');
+
+    mix.scripts([
+        'jquery/dist/jquery.js',
+        'AdminLTE/plugins/iCheck/icheck.min.js',
+        'pnotify/dist/pnotify.js',
+        'vue/dist/vue.js',
+        'vue-resource/dist/vue-resource.js'
+    ], 'public/js/login.js', 'bower_components/');
+
+    // Copy fonts
+    mix.copy([
+        'bower_components/fontawesome/fonts',
+        'bower_components/bootstrap/fonts',
+    ], 'public/fonts');
+
+    mix.copy([
+        'bower_components/AdminLTE/plugins/iCheck/square/blue.png',
+        'bower_components/AdminLTE/plugins/iCheck/square/blue@2x.png',
+    ], 'public/css');
+
+    /**
+     * Add versioning for all asset files
+     */
+    mix.version([
+        'public/css/login.css',
+        'public/js/login.js'
+    ]);
 });
