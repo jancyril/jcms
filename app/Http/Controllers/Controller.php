35ed47d1d2b5b5ec3325c9e6e072fc04bff3d445
Jan Cyril Segubience <jancyrilsegubience@gmail.com>
2016-11-20 10:26:18 +0000

    Methods to return view and response

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,87 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * This will generate the view for admin page.
+     *
+     * @param string $view Path to the view
+     * @param array  $data Data to be passed to the view
+     *
+     * @return Illuminate\View\View
+     */
+    protected function admin(string $view, array $data = [])
+    {
+        return view('admin.'.$view, $data);
+    }
+
+    /**
+     * This will generate the view for user page.
+     *
+     * @param string $view Path to the view
+     * @param array  $data Data to be passed to the view
+     *
+     * @return Illuminate\View\View
+     */
+    protected function user(string $view, array $data = [])
+    {
+        return view('user.'.$view, $data);
+    }
+
+    /**
+     * Method to notify successful operation.
+     *
+     * @param string $message The success message
+     *
+     * @return array
+     */
+    protected function success(string $message, array $data = []): array
+    {
+        $content = [
+            'result' => true,
+            'type' => 'success',
+            'title' => 'Success',
+            'message' => $message,
+        ];
+
+        return empty($data) ? $content : array_merge($content, $data);
+    }
+
+    /**
+     * Method to notify failed operation.
+     *
+     * @param string $message The error message
+     *
+     * @return array
+     */
+    protected function error(string $message, array $data = []): array
+    {
+        $content = [
+            'result' => false,
+            'type' => 'error',
+            'title' => 'Error',
+            'message' => $message,
+        ];
+
+        return empty($data) ? $content : array_merge($content, $data);
+    }
+
+    /**
+     * Method to notify information about an operation.
+     *
+     * @param string $message The informative message
+     *
+     * @return array
+     */
+    protected function info(string $message, array $data = []): array
+    {
+        $content = [
+            'result' => true,
+            'type' => 'info',
+            'title' => 'Heads up',
+            'message' => $message,
+        ];
+
+        return empty($data) ? $content : array_merge($content, $data);
+    }
 }
